691c06569be88f4cd7d6ea56dfc6b1acf37f2b53
Harshal Patil <hpatil0708@gmail.com>
2022-05-26 15:50:42 +0530

    Added another solution for 17

--- java-solutions/medium/0017_Letter-Combinations-of-a-Phone-Number.java
+++ java-solutions/medium/0017_Letter-Combinations-of-a-Phone-Number.java
@@ -37,4 +37,38 @@ class Solution {
 }
 
 /** Another solution is to leverage FIFO nature of queues!
+ * Similar to BFS, we add all the current level strings to queue (this will be removed first)
+ * for each prev level string we make a new current level string and add it to queue
+ * TC: O(4^N) -> the number of possible
+ * SC: O(4^N) -> all strings can be queued (max 4 posibilities per digit, eg. for '7' & '9')
  */
+ class Solution {
+    public List<String> letterCombinations(String digits) {
+        
+        Map<Character, String> dial = Map.of(
+                '2', "abc", '3', "def", '4', "ghi", '5', "jkl",
+                '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+        List<String> ans = new ArrayList<>();
+        
+        if (digits.length() == 0) return ans;
+        
+        // With a queue
+        Queue<String> q = new LinkedList<>();
+        q.add("");  // initialization so the loop starts
+
+        while (!q.isEmpty()) {
+            String s = q.remove();
+
+            if (s.length() == digits.length()) {
+                ans.add(s);
+                continue;
+            }
+            
+            for (char c: dial.get(digits.charAt(s.length())).toCharArray()) {
+                q.add(s + c);
+            }
+        }
+        
+        return ans;
+    }
+}
