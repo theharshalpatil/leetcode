b369233399ea97f5236b34d966c44a8a17668cc3
Harshal Patil <hpatil0708@gmail.com>
2022-05-30 20:35:11 +0530

    Added solution 29

---
+++ java-solutions/medium/0029_Divide-Two-Integers.java
@@ -0,0 +1,17 @@
+/**
+ * Use bit manipulation to determine quotient
+ * TC: O(32)
+ * SC: O(1)
+ */
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == 1 << 31 && divisor == -1) return (1 << 31) - 1;
+        int a = Math.abs(dividend), b = Math.abs(divisor), res = 0;
+        for (int x = 31; x >= 0; x--)
+            if ((a >>> x) - b >= 0) {
+                res += 1 << x;
+                a -= b << x;
+            }
+        return (dividend > 0) == (divisor > 0) ? res : -res;
+    }
+}
