c5525f3e7a215fee15fe62b37dabb57508d6f7ff
Harshal Patil <hpatil0708@gmail.com>
2022-06-02 01:36:01 +0530

    Comments on 387

--- java-solutions/easy/0387_First-Unique-Character-in-a-String.java
+++ java-solutions/easy/0387_First-Unique-Character-in-a-String.java
@@ -24,3 +24,35 @@ class Solution {
         return -1;
     }
 }
+
+/** Two-Pointer Method
+ * Fist pointer holds the curr char if not seen and checks with 2nd pointer.
+ * If second pointer reaches end, then ans is first pointer.
+ * If second pointer never reaches end but no unique char, return -1.
+ * TC: O(N^2)   -> worst case (Still 10x faster on leetcode)
+ * SC: O(1)     -> seen is only len 26
+ */
+class Solution {
+    public int firstUniqChar(String s) {
+        int i = 0, j = 1, len = s.length();
+        boolean[] seen = new boolean[26];
+        while (i < len && j <= len) {
+            if (j == len && !seen[s.charAt(i) - 'a']) return i;
+            if (j == len) return -1;
+            
+            if (seen[s.charAt(i) - 'a']) {
+                i++;
+                j = i+1;
+                continue;
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                seen[s.charAt(i) - 'a'] = true;
+                i++;
+                j = i+1;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+}
