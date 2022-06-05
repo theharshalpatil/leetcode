8352673bfc081c7d0f98485808ce538a08ecbdfd
Harshal Patil <hpatil0708@gmail.com>
2022-06-05 10:48:32 +0530

    Added solution 304

---
+++ java-solutions/medium/0304_Range-Sum-Query-2D-Immutable.java
@@ -0,0 +1,50 @@
+/**
+ * We Precompute the sum of all rows and then while computing, we cumulate this in range
+ * TC: O(M) for lookup. O(MN) for precomputation
+ * SC: O(MN) to store cumulative sum of all rows
+ */
+class NumMatrix {
+    private int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+        dp = new int[matrix.length][matrix[0].length + 1];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                dp[r][c + 1] = dp[r][c] + matrix[r][c];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int row = row1; row <= row2; row++) {
+            sum += dp[row][col2 + 1] - dp[row][col1];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Another smarter approach is to find cumulative sum till all the indices w.r.t. origin
+ * Then we can find sum in region ABCD as (OD - OB - OC + OA)!
+ * TC: O(MN) for precomputation & O(1) Lookup
+ * SC: O(MN) for storing sums w.r.t. origin
+ */
+ class NumMatrix {
+    private int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                dp[r + 1][c + 1] = dp[r + 1][c] + dp[r][c + 1] + matrix[r][c] - dp[r][c];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+}
