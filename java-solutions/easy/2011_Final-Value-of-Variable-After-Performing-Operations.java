901e136526cc8ede9f9268f882acb9485de238b5
Harshal Patil <hpatil0708@gmail.com>
2022-06-18 23:23:53 +0530

    Added solution 2011

---
+++ java-solutions/easy/2011_Final-Value-of-Variable-After-Performing-Operations.java
@@ -0,0 +1,14 @@
+/**
+ * TC: O(N)
+ * SC: O(1)
+ */
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String operation: operations) {
+            if (operation.charAt(1) == '+') x++;
+            else x--;
+        }
+        return x;
+    }
+}
