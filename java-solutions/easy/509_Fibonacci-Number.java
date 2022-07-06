41620ff7341dccabc3a923c86181aad957eba4bf
Harshal Patil <hpatil0708@gmail.com>
2022-07-06 09:37:14 +0530

    Added solution 509

---
+++ java-solutions/easy/509_Fibonacci-Number.java
@@ -0,0 +1,27 @@
+/**
+ * Recursion
+ * TC: O(2^N)
+ * SC: O(1)
+ */
+class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+/** Dynamic Programming
+ * TC: O(N)
+ * SC: O(N)
+ */
+class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for (int i = 2; i <= n; i++) dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
+    }
+}
