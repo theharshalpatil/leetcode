d009b396884987ddf54a897a8253bcd3b7c93b48
Harshal Patil <hpatil0708@gmail.com>
2022-07-14 20:39:19 +0530

    Added solution 122

---
+++ java-solutions/medium/0122_Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -0,0 +1,17 @@
+/**
+ * If tomorrows price is greater than todays price buy/sell otherwise do nothing
+ * TC: O(N)
+ * SC: O(1)
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1])
+                ans += prices[i] - prices[i-1];
+        }
+        
+        return ans;
+    }
+}
