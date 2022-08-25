cfb334d266d11194a7c2fe6efb53a1d2f36fd6c9
Harshal Patil <hpatil0708@gmail.com>
2022-08-25 23:42:37 +0530

    Added solution 1137

---
+++ java-solutions/easy/1137_N-th-Tribonacci-Number.java
@@ -0,0 +1,16 @@
+/** Dynamic Programming
+ * TC: O(N)
+ * SC: O(N)
+ */
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n < 3) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        return dp[n];
+    }
+}
